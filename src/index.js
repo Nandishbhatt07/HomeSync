de42c3058002758c1e8a7093be62bf00efae071e
Nandish Bhatt <122980004+Nandishbhatt07@users.noreply.github.com>
2025-10-12 15:38:42 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HomSyncApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HomSyncApp />
+  </React.StrictMode>
+);
